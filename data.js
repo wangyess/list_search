8ab709264d4525dc57813d8e747d996becfaa975
wangyess <wangyess@github.com>
2017-10-15 09:59:16 +0800

    一阶段更改

--- data.js
+++ data.js
@@ -1,10 +1,14 @@
 //浏览器储存
 ;(function () {
-    window.person={};
-    person.set=function (a,b) {
-        return localStorage.setItem(a,JSON.stringify(b));
+    window.s = {};
+    s.get = function (a) {
+        var sa = localStorage.getItem(a);
+        //用JSON的方式把数据的类型转化回来
+        return JSON.parse(sa);
     };
-    person.get=function (a) {
-        return JSON.parse(localStorage.getItem(a));
+    s.set = function (a, b) {
+        //用JSON的方式转换数据的类型
+        b = JSON.stringify(b);
+        return localStorage.setItem(a, b);
     }
 })();
--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-;(
- function () {
-     person.set('userName','王花花');
-     var ss=person.get('userName');
-     console.log(ss);
- }
-)();
+;(function () {
+    'use strict';
+
+    var task_list;  //任务数据
+    var last_id;    //id名数据
+    init_data();
+
+    //初始化本地数据
+    function init_data() {
+        //获取本地数据
+        task_list = s.get('task_list');
+        last_id = s.get('last_id');
+
+        if (!task_list) {
+            task_list = [];
+            s.set('task_list', task_list);
+        }
+        if (!last_id) {
+            last_id = 0;
+            s.set('last_id', last_id);
+        }
+    }
+
+
+})();
